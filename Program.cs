6c820443506f7c212cf5ded421b3b2c2a886ab04
underfunk <underfunk@TONI-WINDOWS>
2026-02-01 17:02:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Skola_ER_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
