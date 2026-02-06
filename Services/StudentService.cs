b1a6d4d702410fbfb97ee3f343f10aa13c5840e6
tonilingardsson <toni.lingardsson@gmail.com>
2026-02-06 15:47:05 +0100

    Add StudentService class with ShowAllStudents method

---
+++ Services/StudentService.cs
@@ -0,0 +1,17 @@
+﻿using Skola_ER_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skola_ER_Application.Services
+{
+    public static class StudentService
+    {
+        public static void ShowAllStudents(ErSkolaContext context) 
+        {
+
+        }
+    }
+}
