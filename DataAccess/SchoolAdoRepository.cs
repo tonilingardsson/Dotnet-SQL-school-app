2e435b132bdcc50144cf63fa0ab7b7031f66f182
tonilingardsson <toni.lingardsson@gmail.com>
2026-02-15 11:03:59 +0100

    Created SchoolAdoRepository to start implementing ADO.NET on the project

---
+++ DataAccess/SchoolAdoRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace Skola_ER_Application.DataAccess
+{
+    public class SchoolAdoRepository
+    {
+        private readonly string _connectionString; //_tableName
+
+        public SchoolAdoRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private SqlConnection CreateConnection() // _connection
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        // - ShowStaffOverview
+        // - ShowGradesForStudent
+        // - ShowTotalSalaryPerDepartment
+        // - ShowAverageSalaryPerDepartment
+        // - ShowStudentInfoById (SP)
+        // - SetGradeWithTransaction
+    }
+}
